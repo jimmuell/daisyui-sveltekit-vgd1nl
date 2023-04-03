9979ed1848d70f798c9f46e6221ac0b1b07f87a2
jimmuell <jimmuell@aol.com>
2023-04-03 08:54:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-sveltekit-vgd1nl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-sveltekit-vgd1nl)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div>
+<button class="btn">Hello daisyUI</button>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+</div>
+
